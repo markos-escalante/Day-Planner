45539363195dbc17b187219c97b81250f1d9cc3e
Markos Rafael <escalante.markos18@gmail.com>
2021-04-10 20:33:01 -0700

    establish global variables.

---
+++ assets/script.js
@@ -0,0 +1,13 @@
+let currentDay = moment().format("dddd MMMM Do YYYY");
+$("#currentDay").text(currentDay);
+let hourText = [
+    "9:00am",
+    "10:00am",
+    "11:00am",
+    "12:00pm",
+    "1:00pm",
+    "2:00pm",
+    "3:00pm",
+    "4:00pm",
+    "5:00pm"
+];
