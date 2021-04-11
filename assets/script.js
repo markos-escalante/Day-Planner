d010179008637cd9852c6f163a9143298bf19e20
Markos Rafael <escalante.markos18@gmail.com>
2021-04-10 20:38:38 -0700

    continue building our function to set up the block.

--- assets/script.js
+++ assets/script.js
@@ -24,5 +24,21 @@ const blockSetup = () => {
         let saveBtn = $("<button>");
         let hourValue = ["9", "10", "11", "12", "13", "14", "15", "16", "17"];
         
+        timeBlockRow.attr({
+            class: "time-block d-flex bd-highlight p-3 row",
+            id: "timeblock" + i
+        });
+        hourCol.attr("class", "p-2 bd-highlight col-md-3 hour");
+        textCol.attr("class", "p-2 bd-highlight col-md-7");
+        saveCol.attr("class", "p-2 bd-highlight col-md-2");
+        hourBlock.attr({
+            class: "p-2 bd-highlight mt-2",
+            id: "hourBlock" + i
+        });
+        textBlock.attr({
+            class: "text col",
+            id: "textBlock" + i,
+            value: hourValue[i]
+        });
     };
 };
